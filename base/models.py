98737559e0ba6c0e7e2720b30e54a413169e2bec
Farooq <mullafarooq4@gmail.com>
2020-11-03 10:20:13 +0530

    initial commit

---
+++ base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Task(models.Model):
+    title = models.CharField(max_length = 30)
+    deadline = models.DateField()
+    done = models.BooleanField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE, null = True, blank = True)
