ce253051cae5b3eb3d4a79717b2beacfa4ebcb21
Farooq <mullafarooq4@gmail.com>
2020-11-03 15:17:49 +0530

    data display in user home pages

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
 from .models import Task
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='login')
 def home(request):
-    return render(request,'base/home.html')
+    template = 'base/home.html'
+    try:
+        obj = Task.objects.filter(user = request.user)
+    except:
+        obj = 'none'
+    # obj = get_list_or_404(Task,user = request.user)
+    return render(request,template,{'obj': obj})
